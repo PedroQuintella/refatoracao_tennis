deafaa972bfa34c450f9a6a7e2137ed1b0128381
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-04-06 21:18:47 -0300

    Refactoring: métodos substituindo comparações principais
    Substituição das comparações principais nas condicionais da função score ( ), por métodos criados na classe Game, conforme o padrão 'Decompose Conditional'.

--- tennis.py
+++ tennis.py
@@ -5,19 +5,20 @@ class Game:
     thirtyPoints = "Thirty"
     moreThanThirtyPoints = "Deuce"
     tie = "-All"
-    
+
     def __init__(self, player1Name, player2Name):
         self.player1Name = player1Name
         self.player2Name = player2Name
         self.p1points = 0
         self.p2points = 0
-        
+
+
     def won_point(self, playerName):
         if playerName == self.player1Name:
             self.P1Score()
         else:
             self.P2Score()
-    
+
     def score(self):
         result = ""
         if self.tieWithLessThanThreeHits():
@@ -30,79 +31,80 @@ class Game:
             result += self.tie
         if self.tieWithMoreThanTwoHits():
             result = self.moreThanThirtyPoints
-        
+
         P1res = ""
         P2res = ""
-        if (self.p1points > 0 and self.p2points==0):
-            if (self.p1points==1):
+        if self.onlyPlayerOneScored():
+            if (self.p1points == 1):
                 P1res = "Fifteen"
-            if (self.p1points==2):
+            if (self.p1points == 2):
                 P1res = "Thirty"
-            if (self.p1points==3):
+            if (self.p1points == 3):
                 P1res = "Forty"
-            
+
             P2res = "Love"
             result = P1res + "-" + P2res
-        if (self.p2points > 0 and self.p1points==0):
-            if (self.p2points==1):
+
+        if self.onlyPlayerTwoScored():
+            if (self.p2points == 1):
                 P2res = "Fifteen"
-            if (self.p2points==2):
+            if (self.p2points == 2):
                 P2res = "Thirty"
-            if (self.p2points==3):
+            if (self.p2points == 3):
                 P2res = "Forty"
-            
+
             P1res = "Love"
             result = P1res + "-" + P2res
-        
-        
-        if (self.p1points>self.p2points and self.p1points < 4):
-            if (self.p1points==2):
-                P1res="Thirty"
-            if (self.p1points==3):
-                P1res="Forty"
-            if (self.p2points==1):
-                P2res="Fifteen"
-            if (self.p2points==2):
-                P2res="Thirty"
+
+        if self.playerOneWinningWithLessThanFourHits():
+            if (self.p1points == 2):
+                P1res = "Thirty"
+            if (self.p1points == 3):
+                P1res = "Forty"
+            if (self.p2points == 1):
+                P2res = "Fifteen"
+            if (self.p2points == 2):
+                P2res = "Thirty"
             result = P1res + "-" + P2res
-        if (self.p2points>self.p1points and self.p2points < 4):
-            if (self.p2points==2):
-                P2res="Thirty"
-            if (self.p2points==3):
-                P2res="Forty"
-            if (self.p1points==1):
-                P1res="Fifteen"
-            if (self.p1points==2):
-                P1res="Thirty"
+
+        if self.playerTwoWinningWithLessThanFourHits():
+            if (self.p2points == 2):
+                P2res = "Thirty"
+            if (self.p2points == 3):
+                P2res = "Forty"
+            if (self.p1points == 1):
+                P1res = "Fifteen"
+            if (self.p1points == 2):
+                P1res = "Thirty"
             result = P1res + "-" + P2res
-        
-        if (self.p1points > self.p2points and self.p2points >= 3):
+
+        if self.matchPointForPlayerOne():
             result = "Advantage " + self.player1Name
-        
-        if (self.p2points > self.p1points and self.p1points >= 3):
+
+        if self.matchPointForPlayerTwo():
             result = "Advantage " + self.player2Name
-        
-        if (self.p1points>=4 and self.p2points>=0 and (self.p1points-self.p2points)>=2):
+
+        if self.playerOneWins():
             result = "Win for " + self.player1Name
-        if (self.p2points>=4 and self.p1points>=0 and (self.p2points-self.p1points)>=2):
+
+        if self.playerTwoWins():
             result = "Win for " + self.player2Name
         return result
-    
+
     def SetP1Score(self, number):
         for i in range(number):
             self.P1Score()
-    
+
     def SetP2Score(self, number):
         for i in range(number):
             self.P2Score()
-    
+
     def P1Score(self):
-        self.p1points +=1
-    
-    
+        self.p1points += 1
+
     def P2Score(self):
-        self.p2points +=1
-        
+        self.p2points += 1
+
     def tieWithLessThanThreeHits(self):
         if self.p1points == self.p2points and self.p1points < 3:
             return True
@@ -121,4 +123,36 @@ class Game:
 
     def tieWithMoreThanTwoHits(self):
         if self.p1points == self.p2points and self.p1points > 2:
+            return True
+
+    def onlyPlayerOneScored(self):
+        if self.p1points > 0 and self.p2points == 0:
+            return True
+
+    def onlyPlayerTwoScored(self):
+        if self.p2points > 0 and self.p1points == 0:
+            return True
+
+    def playerOneWinningWithLessThanFourHits(self):
+        if self.p1points > self.p2points and self.p1points < 4:
+            return True
+
+    def playerTwoWinningWithLessThanFourHits(self):
+        if self.p2points > self.p1points and self.p2points < 4:
+            return True
+
+    def matchPointForPlayerOne(self):
+        if self.p1points > self.p2points and self.p2points >= 3:
+            return True
+
+    def matchPointForPlayerTwo(self):
+        if self.p2points > self.p1points and self.p1points >= 3:
+            return True
+
+    def playerOneWins(self):
+        if self.p1points >= 4 and self.p2points >= 0 and (self.p1points - self.p2points) >= 2:
+            return True
+
+    def playerTwoWins(self):
+        if self.p2points >= 4 and self.p1points >= 0 and (self.p2points - self.p1points) >= 2:
             return True
